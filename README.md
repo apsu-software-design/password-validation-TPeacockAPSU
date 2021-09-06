a3b5cbe781654c32f7419a08bb21db7555e4a51e
TPeacockAPSU <79169949+TPeacockAPSU@users.noreply.github.com>
2021-09-06 14:52:49 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -8,25 +8,25 @@ All credit goes to the University of Wisconsin for the format of this homework a
 This repo contains code for the password validator. The below questions should be answered regarding your submission!
 
 ## Did you find the JavaScript Learning Tutor useful? If so, what about the tool made it useful? If not, why not?
-> Answer goes here!
+> Yes.
 
 
 ## Do you have any suggestions on how the JavaScript Learning Tutor might be improved?
-> Answer goes here
+> No.
 
 
 ## Did you receive help from any other sources (classmates, etc)? If so, please list who (be specific!).
-> Answer goes here!
+> No.
 
 
 ## Approximately how many hours did it take you to complete this assignment?
-> Answer goes here!
+> 12
 
 
 ## On a scale of 1 (too easy) to 10 (too challenging), how difficult was this assignment?
-> Answer goes here!
+> 6
 
 
 ## Did you encounter any problems in this assignment we should warn students about in the future? How can we make the assignment better?
-> Answer goes here!
+> No real issues, first time using GitHub so that was more confusing than the assignment.
 
--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-const validatePassword = require('./passwordvalidation'); //import the function
 
-// Your code goes here!
+const validatePassword = require('./passwordvalidation'); //Imports the function.
+
+const rl = readline.require('readline').createInterface({
+    intput: process.stdin,
+    output: process.stdout
+});//Reads input from the console.
+
+console.log("TPeacock's PW Validator");
+console.log("Please enter the password you would like to check (type :quit to quit): ");
+
+checkMe();//Function call to start the validation with the given input, if the input is :quit, the program ends.
+
+function checkMe() {
+    readline.question('', isItGood => {
+        if (isItGood == ":quit") {
+            console.log("Thanks for using TPeacock's PW Validator, goodbye!")
+            readline.close();
+        } else if (validatePassword(isItGood)) {
+            console.log("The following password is valid: " + isItGood)
+            checkMe();
+        } else {
+            console.log("The following password is invalid: " + isItGood);
+            checkMe();
+        }
+    });
+}
